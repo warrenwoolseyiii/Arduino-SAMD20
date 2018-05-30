5fcede568c1cd0b586561aaecf62d9dd96e33736
Budgetts Frog <budgettsfrog@protonmail.com>
2018-05-29 17:00:41 -0700

    Implemented the SPI Flash library in Sketch and got it working properly

--- TestApp/Sketch.cpp
+++ TestApp/Sketch.cpp
@@ -1,14 +1,26 @@
 #include <Arduino.h>
+#include <lib/SPIFlash/SPIFlash.h>
+
+uint8_t gLevel = 0x1;
+uint32_t gAddr = 0;
+SPIFlash Flash(2);
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  Flash.initialize();
+  Flash.blockErase32K(gAddr);
 }
 
-uint8_t gLevel = 0x1;
 void loop() {
   digitalWrite(LED_BUILTIN, gLevel);
-  gLevel ^= 0x1;
   delay(500);
-  Serial.println(millis());
+  uint32_t ms = millis();
+  Serial.println(ms);
+  Flash.writeBytes(gAddr, &ms, sizeof(ms));
+  
+  uint32_t test = 0;
+  Flash.readBytes(gAddr, &test, sizeof(test));
+  if( test == ms )  gLevel ^= 0x1;
+  gAddr += sizeof(uint32_t);
 }
