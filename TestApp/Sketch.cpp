4b7e6ef01b0dab8cb1c4ac6b476c19bd8bcf196b
Budgetts Frog <budgettsfrog@protonmail.com>
2018-05-30 17:53:57 -0700

    Got the mutant flume board working (kind of) I am going to run blink the LED for a while to see if it is stable at run time

--- TestApp/Sketch.cpp
+++ TestApp/Sketch.cpp
@@ -22,20 +22,22 @@ void setup() {
   pinMode(LED1, OUTPUT);
   pinMode(RFM_SS, OUTPUT);
   digitalWrite(RFM_SS, HIGH);
-  Flash.initialize();
-  Flash.blockErase32K(gAddr);
-  DEBUGbegin(SERIAL_BAUD);
+  digitalWrite(LED1, HIGH);
+  //Flash.initialize();
+  //Flash.blockErase32K(gAddr);
+  //DEBUGbegin(SERIAL_BAUD);
 }
 
 void loop() {
   digitalWrite(LED1, gLevel);
   delay(500);
   uint32_t ms = millis();
-  DEBUGln(ms);
-  Flash.writeBytes(gAddr, &ms, sizeof(ms));
+  //DEBUGln(ms);
+  //Flash.writeBytes(gAddr, &ms, sizeof(ms));
   
-  uint32_t test = 0;
-  Flash.readBytes(gAddr, &test, sizeof(test));
-  if( test == ms )  gLevel ^= 0x1;
+  //uint32_t test = 0;
+  //Flash.readBytes(gAddr, &test, sizeof(test));
+  //if( test == ms )  
+  gLevel ^= 0x1;
   gAddr += sizeof(uint32_t);
 }
