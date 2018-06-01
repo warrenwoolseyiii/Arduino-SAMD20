414506159d57c2d4d65019005d7056030f71e33e
Budgetts Frog <budgettsfrog@protonmail.com>
2018-06-01 13:50:39 -0700

    Fuses are being corrupted at run time while we switch clocks. Doesn't seem to happen when not using the DFLL

--- TestApp/fuses.c
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-
-#define USER_WORD_0 (uint32_t)0xD9FEC7FF
-#define USER_WORD_1 (uint32_t)0xFFFF3F5D
-
-__attribute__ ((section(".user_word"))) const uint32_t settings[] = {USER_WORD_0 ,USER_WORD_1};
