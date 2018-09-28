5f91858de00d8cf241c82e475a65076c73ed0177
Budgetts Frog <budgettsfrog@protonmail.com>
2018-09-28 14:55:10 -0700

    formatting and clean up

--- src/RTC.c
+++ src/RTC.c
@@ -98,23 +98,24 @@ int64_t secondsRTC()
 void delayRTCSteps( int64_t steps )
 {
     int64_t start = stepsRTC();
-	int64_t rSteps = steps;
-	int64_t remaining = ( RTC_STEPS_PER_SEC - ( stepsRTC() & 0x7FFF ) );
-	do {
-		// If we will be waiting long enough for an overflow interrupt to occur
-		// go to sleep.
-		if( remaining < rSteps ) {
-			rSteps -= remaining;
-			// If Serial is enabled sleep in Idle mode otherwise go into Standby.
+    int64_t rSteps = steps;
+    int64_t remaining = ( RTC_STEPS_PER_SEC - ( stepsRTC() & 0x7FFF ) );
+    do {
+        // If we will be waiting long enough for an overflow interrupt to occur
+        // go to sleep.
+        if( remaining < rSteps ) {
+            rSteps -= remaining;
+            // If Serial is enabled sleep in Idle mode otherwise go into
+            // Standby.
 #if defined( __SAMD20E18__ )
-			if( SERCOM3->USART.CTRLA.bit.ENABLE )
-				sleepCPU( PM_SLEEP_IDLE_CPU_Val );
-			else
-				sleepCPU( PM_SLEEP_STANDBY_Val );
+            if( SERCOM3->USART.CTRLA.bit.ENABLE )
+                sleepCPU( PM_SLEEP_IDLE_CPU_Val );
+            else
+                sleepCPU( PM_SLEEP_STANDBY_Val );
 #endif /* __SAMD20E18__ */
-			remaining = ( RTC_STEPS_PER_SEC - ( stepsRTC() & 0x7FFF ) );
-		}
-	} while( ( stepsRTC() - start ) < steps );
+            remaining = ( RTC_STEPS_PER_SEC - ( stepsRTC() & 0x7FFF ) );
+        }
+    } while( ( stepsRTC() - start ) < steps );
 }
 
 void RTC_IRQHandler()
